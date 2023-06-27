0caa4d8ce398309ee07d3eb563f7dcf66a9494f2
fastlycode <88783219+fastlycode@users.noreply.github.com>
2023-06-28 00:10:13 +0700

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,137 @@
+// document.getElementsByClassName("main-title")[0].style.color = "red";
+// document.getElementById("main-action-button").onclick = function()  {
+// document.getElementById("products").scrollIntoView( {behavior: "smooth"});
+// }
+
+let links = document.querySelectorAll(".header-item > a");
+for (let i= 0; i < links.length; i++){
+    links[i].onclick=function() {// не понимаю в чем тут ошибка
+        document.getElementById(links[i].getAttribute("data-link")).scrollIntoView({behavior:"smooth"});
+    }
+}
+
+const form = document.querySelector('form');
+const fields = form.querySelectorAll('.field');
+let guestname = document.getElementById("guest-name");
+let phone = document.getElementById("phone");
+let question = document.getElementById("question");
+
+document.getElementById("my-form-send").onclick = function() {
+
+    let hasError = false;
+    let MyItems =[guestname, phone, question];
+    MyItems.forEach(item => {
+if (item.value===""){
+    
+
+   item.classList.add('error');
+    console.log("Не все поля заполены");
+    hasError=true;
+
+}else if (!hasError) {
+  item.classList.remove('error');
+ console.log("Форма прошла валидацию");
+    document.getElementById("my-form-send").style.background = "";
+    item.parentElement.style.background = "";
+
+   
+
+}
+
+    });
+
+
+    }
+
+
+fields.forEach((element) => {
+  element.addEventListener('.field', () => {
+    if (element.parentElement.classList.contains('error')) {
+      element.parentElement.classList.remove('error');
+    }
+  });
+});
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  let hasError = false;
+
+  fields.forEach((fields) => {
+    if (!fields.value) {
+      fields.parentElement.classList.add('error');
+      hasError = true;
+    } else {
+      fields.parentElement.classList.remove('error');
+    }
+  });
+
+  if (!hasError) {
+    fields.forEach((element) => {
+      element.value = '';
+    });
+    alert('Спасибо за заказ! Мы скоро свяжемся с вами!');
+  }
+});
+const mobileMenuBtn = document.getElementById("header-burger");
+const mobileMenu = document.querySelector('.mobile-menu');
+console.log(mobileMenu);
+mobileMenuBtn.addEventListener('click', ()=>{
+    mobileMenuBtn.classList.toggle('open-menu');
+    mobileMenu.classList.toggle('active');
+});
+const toggler = document.querySelector('.header-burger')
+const menuMob = document.querySelector('.mobile-menu')
+menuMob.addEventListener('click', (e) => {
+    if (e.target.tagName === 'A') {
+        toggler.classList.remove('open-menu');
+        menuMob.classList.remove('active');
+    }
+});
+
+
+const btnUp = {
+  el: document.querySelector('.btn-up'),
+  show() {
+    // удалим у кнопки класс btn-up_hide
+    this.el.classList.remove('btn-up_hide');
+  },
+  hide() {
+    // добавим к кнопке класс btn-up_hide
+    this.el.classList.add('btn-up_hide');
+  },
+  addEventListener() {
+    // при прокрутке содержимого страницы
+    window.addEventListener('scroll', () => {
+      // определяем величину прокрутки
+      const scrollY = window.scrollY || document.documentElement.scrollTop;
+      // если страница прокручена больше чем на 400px, то делаем кнопку видимой, иначе скрываем
+      scrollY > 400 ? this.show() : this.hide();
+    });
+    // при нажатии на кнопку .btn-up
+    document.querySelector('.btn-up').onclick = () => {
+      // переместим в начало страницы
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      });
+    }
+  }
+}
+
+btnUp.addEventListener();
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
